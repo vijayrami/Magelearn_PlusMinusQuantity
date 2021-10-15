8c5a5deb68e37f322b8985e39245855a8ed6cf65
vijayrami <vijaymrami@gmail.com>
2021-10-15 11:21:12 +0530

    First Commit

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/minicart/item/default.html': 'Magelearn_PlusMinusQuantity/template/minicart/item/default.html',
+            'sidebar': 'Magelearn_PlusMinusQuantity/js/sidebar',
+            'Magento_Checkout/js/view/minicart': 'Magelearn_PlusMinusQuantity/js/view/minicart'
+        }
+    }
+};
